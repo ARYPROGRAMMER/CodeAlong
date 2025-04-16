847d7408a3214f899fa5cb5afa5d4a7579cef76e
Arya Pratap Singh <notaryasingh@gmail.com>
2025-04-16 20:08:05 +0530

    feat: add convex
    Signed-off-by: Arya Pratap Singh <notaryasingh@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import { Button } from "@/components/ui/button";
 import {
   SignedOut,
   SignInButton,
@@ -6,18 +5,11 @@ import {
   SignedIn,
   UserButton,
 } from "@clerk/nextjs";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="m-10">
-      <SignedOut>
-        <SignInButton />
-        <SignUpButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
+    Home Page
     </div>
   );
 }
