95afd5d218a6e6fda6dca387f5846b02768f9daa
Arya Pratap Singh <notaryasingh@gmail.com>
2025-04-19 14:20:07 +0530

    role hook complete
    Signed-off-by: Arya Pratap Singh <notaryasingh@gmail.com>

--- src/components/DashboardButton.tsx
+++ src/components/DashboardButton.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import { Button } from "./ui/button";
+import { SparklesIcon } from "lucide-react";
+import { useUserRole } from "@/hooks/useUserRole";
 
 function DashboardButton() {
+  const { isCandidate, isLoading } = useUserRole();
+
+  if (isCandidate || isLoading) return null;
+
   return (
-    <div>DashboardButton</div>
-  )
+    <Link href="/dashboard">
+      <Button className="gap-2 font-medium" size={"sm"}>
+        <SparklesIcon className="size-4" />
+        Dashboard
+      </Button>
+    </Link>
+  );
 }
 
-export default DashboardButton
+export default DashboardButton;
